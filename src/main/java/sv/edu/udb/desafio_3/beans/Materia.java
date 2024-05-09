e75bc6222163c53893669cd6bb56593b6c4a5cfe
Menendez2004 <nitrotruelo@gmail.com>
2024-05-08 19:13:19 -0600

    beans added

---
+++ src/main/java/sv/edu/udb/desafio_3/beans/Materia.java
@@ -0,0 +1,28 @@
+package sv.edu.udb.desafio_3.beans;
+
+public class Materia {
+    private int idMateria;
+    private String materia;
+
+    public Materia(String materia){
+        this.idMateria = idMateria;
+        this.materia = materia;
+    }
+    // Métodos Setter
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public void setMateria(String materia) {
+        this.materia = materia;
+    }
+
+    // Métodos Getter
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public String getMateria() {
+        return materia;
+    }
+}
